c767b4cd1d5f4e4555a880328c9edbffb343761d
Mikio Morii <morii.mikio@gmail.com>
2023-06-01 08:02:23 +0000

    add_stat

--- preproc/add_stat.py
+++ preproc/add_stat.py
@@ -20,6 +20,7 @@ import sys
 import pandas as pd
 from akarilib import calc_norm_in_row_of_dataframe
 from akarilib import calc_stat_in_row_of_dataframe
+from akarilib import calc_feature_in_row_of_dataframe
 
 
 indir = os.environ["AKARI_ANA_DIR"]
@@ -29,11 +30,19 @@ print(data_df)
 
 data_sel_df = data_df.drop(["file", "tzl_x", "tzl_y",
                             "crval1", "crval2", "ra", "dec"], axis=1)
-data_norm_df = data_sel_df.apply(calc_norm_in_row_of_dataframe, axis=1)
+data_norm_df = data_sel_df.apply(
+    calc_norm_in_row_of_dataframe, axis=1)
 print(data_norm_df)
-data_stat_df = data_norm_df.apply(calc_stat_in_row_of_dataframe, axis=1)
+data_stat_df = data_norm_df.apply(
+    calc_stat_in_row_of_dataframe, axis=1)
 print(data_stat_df)
-data_add_df = pd.concat([data_df, data_norm_df, data_stat_df], axis=1)
+data_feature_df = data_norm_df.apply(
+    calc_feature_in_row_of_dataframe, axis=1)
+
+data_add_df = pd.concat([data_df,
+                         data_norm_df,
+                         data_stat_df,
+                         data_feature_df], axis=1)
 print(data_add_df)
 
 outdir = indir

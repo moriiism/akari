191fd751a204aa1a298cd5513cf5a6d3c4182258
Mikio Morii <morii.mikio@gmail.com>
2024-05-29 17:45:47 +0900

    add shell

---
+++ setup/setup_st20070102G2_G000.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+branch_name=develop
+data_name=st20070102G2_G000
+model_name=st20070102G2_G000
+
+export AKARI_DATA_DIR=/home/morii/work/akari/data/${data_name}
+export AKARI_TOOL_DIR=/home/morii/work/github/moriiism/akari
+export PYTHONPATH=$AKARI_TOOL_DIR
+export AKARI_ANA_DIR=/home/morii/work/akari/ana/${branch_name}/${data_name}
+export MODEL_DIR=/home/morii/work/akari/ana/${branch_name}/${model_name}
+
+echo "AKARI_DATA_DIR="$AKARI_DATA_DIR
+echo "AKARI_TOOL_DIR="$AKARI_TOOL_DIR
+echo "PYTHONPATH="$PYTHONPATH
+echo "AKARI_ANA_DIR="$AKARI_ANA_DIR
+echo "MODEL_DIR="$MODEL_DIR
+
+termtitle="BRANCH=${branch_name}, DATA=${data_name}, MODEL=${model_name}"
+PROMPT_COMMAND='echo -ne "\033]0;${termtitle}\007"'
+echo "setup done."
